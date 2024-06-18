3e017b6385448969a58fe9aec0d8a451b9041412
John Elvis Arbaiza Velásquez <102486877+JohnArbaiza1@users.noreply.github.com>
2024-06-18 01:17:54 -0600

    Funcionalidad rutas del usuario

--- src/Controllers/EventosUser.controller.js
+++ src/Controllers/EventosUser.controller.js
@@ -3,7 +3,7 @@
 //importamos la conexion
 import { pool } from "../db/db.js";
 
-//Controlador encargado de mostrar evento segun el usuario que lo creo
+//Controlador encargado de mostrar los eventos segun el usuario que lo creo
 export const getUserEvento = async (req, res) => {
     try {
         //const userId = req.params.idUsuario;
@@ -20,4 +20,53 @@ export const getUserEvento = async (req, res) => {
         res.status(500).json({ message: 'Algo salió mal' });
     }
 };
+
+//Controlador encargado de mostrar un evento en especifico segun el usuario que lo creo
+export const getUserEvent = async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM eventos WHERE idUsuario = ? AND idEvento = ?', [req.params.idUsuario, req.params.idEvento]);
+        // Validamos en caso de que no exista el id buscado
+        if (rows.length <= 0) {
+            return res.status(404).json({
+                message: 'Eventos Not Found'
+            });
+        }
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Algo salió mal'
+        });
+        console.log(error);
+    }
+};
+
+//Controlador encargado de actualizar segun el uduario
+export const updateEventoUser = async(req,res) =>{
+    //Extraemos el valor de los siguientes parámetros
+    const {idUsuario,idEvento} = req.params;
+    const {nombreEvento,asistentes,Descripción,ubicacion,Fecha,Username,idCategoria,categoria,img, fechaCreacion} = req.body;
+    try {
+        const [results] = await pool.query('UPDATE eventos SET nombreEvento = IFNULL(?, nombreEvento), asistentes = IFNULL(?, asistentes), Descripción = IFNULL(?, Descripción), ubicacion = IFNULL(?, ubicacion)' +
+            ',Fecha = IFNULL(?, Fecha), Username = IFNULL(?, Username), idCategoria = IFNULL(?, idCategoria), categoria = IFNULL(?, categoria), img = IFNULL(?, img), fechaCreacion = IFNULL(?, fechaCreacion)'+
+            '  WHERE idUsuario = ? AND idEvento = ?',
+            [nombreEvento, asistentes, Descripción, ubicacion, Fecha, Username, idCategoria, categoria, img, fechaCreacion, idUsuario, idEvento]
+        );
+        
+        if(results.affectedRows === 0 ) return res.status(404).json({
+            message: 'Eventos Not Found'
+        });
+
+        const [rows] = await pool.query('SELECT * FROM eventos WHERE idUsuario = ? AND idEvento = ? ', [idUsuario,idEvento]);
+        res.json(rows[0]);
+    
+    } catch (error) {
+        res.status(500).json({
+            message: 'Algo salio mal'
+        })
+        console.log(error);
+    }
+};
+
+
+
 
--- src/routes/EventosUser.routes.js
+++ src/routes/EventosUser.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import {
-    getUserEvento
+    getUserEvento,
+    getUserEvent,
+    updateEventoUser
 } from '../Controllers/EventosUser.controller.js'
 
 const router = Router();
 
-router.get('/EventosUser/:idUsuario',getUserEvento)
+router.get('/EventosUser/:idUsuario',getUserEvento);
+router.get('/EventosUser/:idUsuario/:idEvento', getUserEvent);
+router.patch('/EventosUser/:idUsuario/:idEvento', updateEventoUser);
 
 export default router;
